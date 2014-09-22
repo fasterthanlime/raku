8b19e855d607a54fefd2b2c800a8137728ac8a41
Amos Wenger <fasterthanlime@gmail.com>
2014-09-22 22:15:05 +0200

    Switch to published version of dieter-sass

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [hiccup "1.0.5"]
-                 [dieter "0.4.1"]]
-  :resource-paths ["resources/sass-0.0.1-standalone.jar"]
+                 [dieter "0.4.1"]
+                 [org.clojars.fasterthanlime/dieter-sass "0.0.1"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler raku.handler/app}
   :profiles
